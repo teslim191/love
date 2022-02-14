1621cfd50ce49c8a32d3e3c802e0b91c824d37e1
Teslim <teslimjimoh191@gmail.com>
2022-02-14 20:13:46 +0100

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
+
+dotenv.config({path: './config/config.env'});
 const app = express();
 
 app.use('/', require('./routes/index'));
 
 
-const PORT = 4000
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
