816ee2cd75f6c1eaed690d93b22a3ee47293d1c8
Teslim Jimoh <teslimjimoh191@gmail.com>
2025-01-25 18:26:08 +0100

    Update message.js

--- api/message.js
+++ api/message.js
@@ -2,15 +2,15 @@ const message = {
   ifemi: {
     title: "Ifemi",
     message:
-      "They say, Love is not by explanation but by demonstaration so let me prove to you that i am the one for you. with my actions i shall reach out and touch your very soul. to let you know that you deserve to love and be loved as you are. so here is to me loving you just as you are watching you blossom in all your beauty while i stand by you cheering you on. I love you from the depth of my heart, Halima",
+      "They say, Love is not by explanation but by demonstration so let me prove to you that I am the one for you. With my actions, I shall reach out and touch your very soul. to let you know that you deserve to love and be loved as you are. so here is to me loving you just as you are watching you blossom in all your beauty while I stand by you cheering you on. I love you from the depth of my heart",
   },
   Ayomi:{
       title: 'Ayomi',
-      message: 'From the skies, to the moon, past the galaxies, my love for you will remain. Love from Teslim'
+      message: 'From the skies to the moon, past the galaxies, my love for you will remain. Love from Teslim'
   },
   Idunnumi:{
       title: 'Idunnumi',
-      message: 'There are many ways people define love. some say its a decision. some say it is an emotion. but i like to think it is more than this. love is a person, it is perfection. love is art, it is seraphic and when i say love, i mean you. love is you Halima'
+      message: 'There are many ways people define love. some say it's a decision. some say it is an emotion. but I like to think it is more than this. love is a person, it is perfection. love is art, it is seraphic and when I say love, I mean you. love is you babe'
   }
 };
 
